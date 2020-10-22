f9fe8195c4d576bf6b2961beb1504d11926e2adb
juandiaz960 <42452344+juandiaz960@users.noreply.github.com>
2020-10-22 12:38:01 -0500

    Base archivos del proyecto

---
+++ src/main/java/edu/eci/arsw/exam/AppWheater.java
@@ -0,0 +1,16 @@
+package edu.eci.arsw.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.List;
+
+@SpringBootApplication
+public class AppWheater {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWheater.class, args);
+	}
+
+}
